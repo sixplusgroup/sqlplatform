202c02bd4f22538d17256b92291d895716ae7c95
zheyuanlin <lzy9986@126.com>
2024-09-26 21:04:36 +0800

    fix: fix the SSL to smtp server

--- src/test/java/com/example/sqlexercise/service/ConstantsOfTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.sqlexercise.service;
-
-/**
- * 测试代码中用到的常量，可根据本地数据库中数据进行相应修改
- */
-class ConstantsOfTest {
-    static final String USER_ID = "38748ac0-1f40-4ada-b5b7-ee4df2e20128";
-    static final String USER_EMAIL = "auoy23@qq.com";
-}
